c902bf795b842d1853f215f88d5815b9ff5dd9ed
Dillon Coleman <coleman399@gmail.com>
2021-11-17 14:36:12 -0700

    added getSongs function to retreive data from MySQL

--- dcc-MusicLibrary-JSX/musiclibrary/src/App.js
+++ dcc-MusicLibrary-JSX/musiclibrary/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: []
+    }
+  }
 
+  componentDidMount(){
+    this.getSongs();
+  }
+
+  async getSongs() {
+    let response = await axios.get('http://127.0.0.1:8000/music/')
+    console.log(response.data);
+    this.setState({
+      songs: response.data
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.songs.map(song => {
+          return (
+            <ul className="list-group">
+              <li>title: {song.title}</li>
+              <li>artist: {song.artist}</li>
+              <li>album: {song.album}</li>
+              <li>genre: {song.genre}</li>
+              <li>-----------------------------------------------</li>
+            </ul> 
+          )
+        })})
+      </div>
+    );
+  }
+}
 export default App;
