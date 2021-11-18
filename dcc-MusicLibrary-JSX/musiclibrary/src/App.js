69924ff32c7440ddb501a36c62d9c0be4c3464c2
Dillon Coleman <coleman399@gmail.com>
2021-11-17 17:53:10 -0700

    Added deleteSong function to app.js

--- dcc-MusicLibrary-JSX/musiclibrary/src/App.js
+++ dcc-MusicLibrary-JSX/musiclibrary/src/App.js
@@ -23,10 +23,29 @@ class App extends Component {
     });
   }
 
+  async deleteSong(id) {
+    let response = await axios.delete('http://127.0.0.1:8000/music/' + id + '/')   
+    console.log(response.data);
+    this.state.songs = this.deletedSong(this.state.songs, id)
+    this.setState({
+      songs: response.data
+    });
+  }
+
+  deletedSong(songs, id) {
+    let newSongs = [];
+    songs.map((song) => {
+      if (song.id !== id){
+        newSongs.push(songs)
+      }
+    })
+    return newSongs;
+  }
+
   render() {
     return (
       <div className="container-fluid">
-        <SongTable {...this.state}/>
+        <SongTable {...this.state} deleteSong = {this.deleteSong}/>
       </div>
     );
   }
--- dcc-MusicLibrary-JSX/musiclibrary/src/components/SongTable/SongTable.jsx
+++ dcc-MusicLibrary-JSX/musiclibrary/src/components/SongTable/SongTable.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 
 const SongTable = (props) => {
-        return (
-            <div>
-                <table className="table">
-                        <th scope="col">title</th>
-                        <th scope="col">artist</th>
-                        <th scope="col">album</th>
-                        <th scope="col">genre</th>
-                        <th scope="col">release date</th>
-                    <tbody>
-                        {props.songs.map(song => {
-                            return (
-                                <tr>
-                                    <td>{song.title}</td>
-                                    <td>{song.artist}</td>
-                                    <td>{song.album}</td>
-                                    <td>{song.genre}</td>
-                                    <td>{song.release_date}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>    
-                </table> 
-            </div>
+    return (
+        <div>
+            <table className="table">
+                    <th scope="col">title</th>
+                    <th scope="col">artist</th>
+                    <th scope="col">album</th>
+                    <th scope="col">genre</th>
+                    <th scope="col">release date</th>
+                <tbody>
+                    {props.songs.map(song => {
+                        return (
+                            <tr>
+                                <td>{song.title}</td>
+                                <td>{song.artist}</td>
+                                <td>{song.album}</td>
+                                <td>{song.genre}</td>
+                                <td>{song.release_date}</td>
+                                <td><button onClick={props.deleteSong.bind(this, song.id)}>Delete</button></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>    
+            </table> 
+        </div>
     )
 }
 
